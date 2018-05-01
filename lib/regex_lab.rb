61bc56b41db821a096f8d9a593322137395c92c8
Laura Messa Travin <laura.travin@gmail.com>
2018-05-01 18:58:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,9 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  array = [ ]
+  array = text.scan(\W\w{5}\b)
+  array
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
